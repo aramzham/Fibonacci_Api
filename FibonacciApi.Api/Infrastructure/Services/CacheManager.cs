9c9f1a3e18ad1076c426722924029c4cdb00c889
Aram Zhamkochyan <aramzham@hotmail.com>
2022-09-12 23:58:36 +0400

    switched to PeriodicTimer for CacheManager.cs

--- FibonacciApi.Api/Infrastructure/Services/CacheManager.cs
+++ FibonacciApi.Api/Infrastructure/Services/CacheManager.cs
@@ -3,19 +3,16 @@ using FibonacciApi.Api.Infrastructure.Services.Interfaces;
 
 namespace FibonacciApi.Api.Infrastructure.Services;
 
-public class CacheManager : ICacheManger
+public class CacheManager : ICacheManger, IDisposable
 {
-    private System.Timers.Timer _cacheInvalidationTimer;
+    private BackgroundTask _task;
     
     private readonly Dictionary<int, ulong> _cache = new() { { 0, 0 }, { 1, 1 } };
 
     public CacheManager(IConfiguration configuration)
     {
-        _cacheInvalidationTimer = new System.Timers.Timer();
-        _cacheInvalidationTimer.Elapsed += OnTimerElapsed;
-        _cacheInvalidationTimer.Interval =
-            ulong.TryParse(configuration["CacheLifeTimeInMs"], out var clt) ? clt : 60000;
-        _cacheInvalidationTimer.Enabled = true;
+        _task = new BackgroundTask(TimeSpan.FromMilliseconds(int.TryParse(configuration["CacheLifeTimeInMs"], out var clt) ? clt : 60000));
+        _task.Start(OnTimerElapsed);
     }
     
     public bool Contains(int i) => _cache.ContainsKey(i);
@@ -24,10 +21,15 @@ public class CacheManager : ICacheManger
 
     public void Set(ulong value, int index) => _cache[index] = value;
     
-    private void OnTimerElapsed(object? sender, ElapsedEventArgs e)
+    private void OnTimerElapsed()
     {
         _cache.Clear();
         _cache.Add(0, 0);
         _cache.Add(1, 1);
+    }
+
+    public void Dispose()
+    {
+        _task.Stop();
     }
 }
